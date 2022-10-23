7f79c2fc376382b96ae3ee720b292a068a6a4ca1
Anastasia Kiseleva <aakkiseleva@gmail.com>
2022-10-23 19:15:08 +0300

    Доработки

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,44 +1,75 @@
 import data from './data.json' assert {type: 'json'};
 import flatpickr from 'flatpickr';
+import { Russian } from "flatpickr/dist/l10n/ru.js";
 
+const animContainer = document.querySelector('.anim');
+const circleButton = animContainer.querySelector('.anim__circle');
 
-const circleButton = document.querySelector('.page__circle');
 const markSelect = document.getElementById('mark');
 const modelSelect = document.getElementById('model');
 const yearSelect = document.getElementById('year');
+
 const form = document.querySelector('.auto__form');
-const deliveryButton = document.querySelector('.auto__delivery');
-const date = document.querySelector('.auto__date');
+const deliveryButton = form.querySelector('.auto__button');
+const date = form.querySelector('.auto__date');
+const againLink = document.querySelector('.auto__link');
+const text = document.querySelector('.auto__text');
+const textSpan = text.querySelectorAll('span');
 
-modelSelect.innerHTML = `<option value=''>Модель</option>`;
-yearSelect.innerHTML = `<option value=''>Год</option>`;
-markSelect.innerHTML = `<option value=''>Марка</option>`;
+const fragment = document.createDocumentFragment();
 
-// Calendar
+// Функция для очистки массива от повторяющихся значений
+const makeUniq = (arr) => arr.filter((el, id) => arr.indexOf(el) === id);
+
+// Функция создания списка в селекте
+const makeList = (el, select) => {
+  const newOption = document.createElement('option');
+  newOption.value = el;
+  newOption.textContent = el;
+  fragment.append(newOption);
+  select.append(fragment);
+};
+
+// Инициализация календаря
 const addCalendar = (min, max) => {
   flatpickr(document.getElementById('calendar'), {
     dateFormat: 'd.m.Y',
     minDate: min,
     maxDate: max,
     disableMobile: 'true',
+    inline: true,
+    locale: Russian,
   });
 };
 
-const clickCircleButton = () => {
-  circleButton.addEventListener('click', () => {
+// Появление календаря после нажатия на кнопку "Доставить"
+deliveryButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  document.querySelector('.flatpickr-calendar').style.display = 'block';
+});
+
+// Появление кнопки "Нажать" после анимации квадратов
+document.querySelector('.anim__square--1').addEventListener('animationend', () => {
+  circleButton.classList.add('animate');
+})
+
+// Обработчики для кнопки "Нажать"
+const onCircleButtonAddForm = () => {
   form.classList.add('active');
-  })
+  form.reset();
+  circleButton.classList.remove('animate');
+};
+
+const onCircleButtonClick = () => {
+  circleButton.addEventListener('click', onCircleButtonAddForm)
 }
 
-setTimeout(clickCircleButton, 4500);
+// Клик по кнопке "Нажать" когда анимация поворота закончилась
+circleButton.addEventListener('animationend', onCircleButtonClick);
 
 
+// Добавление в массив данных о марке авто
 const markArray = [];
-
-const makeUniq = (arr) => {
-  return arr.filter((el, id) => arr.indexOf(el) === id);
-}
-
 
 data.forEach((item) => {
   markArray.push(item.mark);
@@ -46,11 +77,12 @@ data.forEach((item) => {
 
 const marks = makeUniq(markArray);
 
+// Вывод в селект данных о марке авто
 for (let i = 0; i < marks.length; i++) {
-  markSelect.innerHTML += `<option value=${marks[i]}>${marks[i]}</option>`;
+  makeList(marks[i], markSelect);
 }
 
-// Выбор марки
+// Выбор модели при изменении марки
 markSelect.addEventListener('change', (evt) => {
   const modelArray = [];
 
@@ -66,7 +98,7 @@ markSelect.addEventListener('change', (evt) => {
   yearSelect.innerHTML = `<option value=''>Год</option>`;
 
   for (let i = 0; i < models.length; i++) {
-    modelSelect.innerHTML += `<option value=${models[i]}>${models[i]}</option>`;
+    makeList(models[i], modelSelect);
   }
 });
 
@@ -82,40 +114,57 @@ modelSelect.addEventListener('change', (evt) => {
 
   const years = makeUniq(yearArray);
   yearSelect.innerHTML = `<option value=''>Год</option>`;
+
   for (let i = 0; i < years.length; i++) {
-    yearSelect.innerHTML += `<option value=${years[i]}>${years[i]}</option>`;
+    makeList(years[i], yearSelect);
   }
 });
 
 // Выбор даты доставки после выбора года выпуска
-yearSelect.addEventListener('change', (evt) => {
-  const deliveryArray = [];
+yearSelect.addEventListener('change', () => {
   for (let i = 0; i < data.length; i++) {
 
-    if(data[i].year === evt.target.value) {
-      deliveryArray.push(data[i].delivery.split('-'));
-
-      for (let j = 0; j < deliveryArray.length; j++) {
-        const newArray = [].concat(deliveryArray[0], deliveryArray[j]);
-        console.log(newArray);
-        addCalendar(newArray[0], newArray[1]);
-      }
-
+    if(data[i].year === yearSelect.value && data[i].model === modelSelect.value) {
+      const deliveryArray = data[i].delivery.split('-');
+      addCalendar(deliveryArray[0], deliveryArray[1]);
+      document.querySelector('.flatpickr-calendar').style.display = 'none';
     }
   }
-})
+});
+
 
 // Появление кнопки доставка, если заполнены все поля
 form.addEventListener('change', () => {
+
   if (markSelect.value !== '' && modelSelect.value !== '' && yearSelect.value !== '') {
     deliveryButton.classList.add('active');
   } else {
     deliveryButton.classList.remove('active');
+    document.querySelector('.flatpickr-calendar').style.display = 'none';
+
   }
+
+  if (date.value !== '') {
+    form.classList.remove('active'); // Скрытие формы
+    animContainer.style.display = 'none';
+    text.style.display = 'block';
+    document.querySelector('.flatpickr-calendar').style.display = 'none';
+    deliveryButton.classList.remove('active');
+  };
+
+  const select = document.querySelectorAll('.auto__select');
+  for (let i = 0; i < textSpan.length; i++) {
+    textSpan[i].textContent = select[i].value;
+  }
+
+  // Удаление клика с кнопки "Нажать", чтобы клик был после анимации
+  circleButton.removeEventListener('click', onCircleButtonAddForm)
 });
 
-// Появление календаря
-deliveryButton.addEventListener('click', () => {
-  date.classList.add('active');
-  date.style.opacity = '1';
+
+// Появление анимации квадратов после клика на "Начать заново"
+againLink.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  document.querySelector('.auto__text').style.display = 'none';
+  animContainer.style.display = 'flex';
 });
